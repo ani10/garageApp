e12fc18ad0dec6a4b551bd6f967093a1e06daff0
ani10 <ani.expo10@gmail.com>
2020-10-31 15:04:31 +0530

    mock data ready

--- src/app/car-size/car-size.page.ts
+++ src/app/car-size/car-size.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Size {
+  name: String;
+  price: Number; 
+}
+
 @Component({
   selector: 'app-car-size',
   templateUrl: './car-size.page.html',
@@ -7,11 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarSizePage implements OnInit {
 
-  sizes = [];
+  sizes = Array<Size>();
 
   constructor() { }
 
   ngOnInit() {
+    this.sizes = [{
+      "name": "small",
+      "price": 1
+    },
+    {
+      "name": "sedan",
+      "price": 1.5
+    },
+    {
+      "name": "minivan",
+      "price": 1.75
+    },
+    {
+      "name": "hatchback",
+      "price": 2
+    },
+    {
+      "name": "CUV",
+      "price": 2.5
+    },
+    {
+      "name": "Van",
+      "price": 2.75
+    },
+    {
+      "name": "Supercar",
+      "price": 3
+    },
+    {
+      "name": "Pickup Truck",
+      "price": 3.5
+    },
+    {
+      "name": "SUV",
+      "price": 4
+    }]
   }
 
 }
